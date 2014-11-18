a2a64e5806628ad16bde37672007bafda4a89444
roryg11 <Rory.C.Grant@gmail.com>
2014-11-18 13:59:15 -0800

    initial push of final docs

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,14 @@
 class PagesController < ApplicationController
+  def verse1
+  end
 
+  def verse2
+  end
+
+  def verse3
+  end
+
+  def verse4
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  root to: "pages#index"
+  root to: "pages#verse1"
+  get 'Verse2' => 'pages#verse2', as: :verse2
+  get 'Verse3' => 'pages#verse3', as: :verse3
+  get 'Verse4' => 'pages#verse4', as: :verse4
+  get 'Verse1' => 'pages#verse1', as: :verse1
 end
